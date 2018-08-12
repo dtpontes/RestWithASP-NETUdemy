6b9dfac65794f887d86ddb3b481d399792ec639e
Daniel <dtpontesinfo@gmail.com>
2018-08-11 23:26:18 -0300

    Start development in rest

---
+++ RestWithASPNETUdemy-02-Using-different-verbs/RestWithASPNETUdemy/Model/Context/MySqlContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RestWithASPNETUdemy.Model.Context
+{
+    public class MySqlContext : DbContext
+    {
+        protected MySqlContext()
+        {
+        }
+
+        protected MySqlContext(DbContextOptions<MySqlContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Person> Person { get; set; }
+    }
+}
--- RestWithASPNETUdemy-02-Using-different-verbs/RestWithASPNETUdemy/Startup.cs
+++ RestWithASPNETUdemy-02-Using-different-verbs/RestWithASPNETUdemy/Startup.cs
@@ -4,26 +4,36 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using RestWithASPNETUdemy.Model.Context;
+using RestWithASPNETUdemy.Services;
+using RestWithASPNETUdemy.Services.Implementattions;
 
 namespace RestWithASPNETUdemy
 {
     public class Startup
     {
+        public IConfiguration Configuration { get; }
+
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
         }
-
-        public IConfiguration Configuration { get; }
+        
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            var connection = Configuration["MySqlConnection:MySqlConnectionString"];
+
+            services.AddDbContext<MySqlContext>(options => options.UseMySql(connection));
             services.AddMvc();
+
+            services.AddScoped<IPersonService, PersonServiceImpl>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
